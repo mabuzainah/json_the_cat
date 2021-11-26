a29042d497fd1aa12360b78cfe08ab6805a5da09
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-11-26 10:07:14 -0500

    Update breedFetcher.js
    Pulled in the description of the object.

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,19 +4,16 @@ const request = require('request');
 //https://api.thecatapi.com/v1/breeds/search?q=Siberian
 
 request('https://api.thecatapi.com/v1/breeds/search?q=Siberian', (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //console.log('error:', error); // Print the error if one occurred
+  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
-  console.log("BELOW IS THE TYPE OF THE BODY");
-  console.log(typeof body);
+  //console.log("BELOW IS THE TYPE OF THE BODY");
+  //console.log(typeof body);
   const data = JSON.parse(body);
   console.log(data);
   console.log(typeof data);
-  for (let i of Object.keys(data) ) {
-      if (i === 'description') {
-          console.log(data.description);
-      }
-  }
+  //accessing the description of the first Object returned. 
+  console.log(data[0].description);
 });
 
 
